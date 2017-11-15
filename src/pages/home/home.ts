9075d229d9af96f5c63547ac042f7de151f9f9d9
CaioGM <caiogregoriom@gmail.com>
2017-11-15 14:31:17 -0200

    comiti caio

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,53 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    messagesUsuario = [];
+    messagesWatson = [];
+    chatBox = "";
+    
+    apiUrl = 'http://localhost:8080/chatinteligente/rest/conversacao/';
 
+  constructor(public navCtrl: NavController, public http: HttpClient) {
+      
+      var nome = this.getUrlParametros("nome");
+      var perfil = this.getUrlParametros("perfil");
+     
+     // alert(nome + " " + perfil);
+  
   }
-
+  
+    send(message) {
+   
+        if(message && message != "") {
+            this.messagesUsuario.push(message);
+            this.messagesWatson.push("");
+            return new Promise(resolve => {
+                this.http.post(this.apiUrl,JSON.stringify(message)).subscribe(data => {
+                    resolve(data);
+                    alert(data);
+                    this.messagesWatson.push(data);
+                    this.messagesUsuario.push("");
+                    }, err => {
+                    console.log(err)
+                    });
+                });
+        }
+            
+            
+            this.chatBox = "";
+        }
+    
+    getUrlParametros(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
 }
+        
+    }
+
+
