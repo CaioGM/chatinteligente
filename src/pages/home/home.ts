6ea1ccc80a8bffae84377b56da87248341a31956
CaioGM <caiogregoriom@gmail.com>
2017-11-30 11:20:21 -0200

    alteracao tela

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -12,40 +12,65 @@ export class HomePage {
     messagesWatson = [];
     chatBox = "";
     
+     msg = [{
+      content :  'Bem-vindo!',
+      position : 'left',
+      senderName : 'Watson'
+    }];
+    
+    //apiUrl = 'http://54.233.240.217:8080/chatinteligente/rest/conversacao/';
     apiUrl = 'http://localhost:8080/chatinteligente/rest/conversacao/';
 
   constructor(public navCtrl: NavController, public http: HttpClient) {
       
       var nome = this.getUrlParametros("nome");
       var perfil = this.getUrlParametros("perfil");
-     
+      
+      /*if(!nome){
+          
+          this.setMsgWatson("Bem-vindo!");
+          
+      }else{
+          
+          this.setMsgWatson("Bem-vindo " + nome);
+          
+      }*/
      // alert(nome + " " + perfil);
   
   }
   
     send(message) {
-   
+        this.setMsgUsuario(message);
+        this.chatBox = "";
+        //window.location.href='#ancora';
+        document.getElementById("inputChat").focus();
         if(message && message != "") {
-            this.messagesUsuario.push(message);
-            this.messagesWatson.push("");
+            //this.messagesUsuario.push(message);
+            //this.messagesWatson.push("");
             return new Promise(resolve => {
                 this.http.post(this.apiUrl,JSON.stringify(message)).subscribe(data => {
                     resolve(data);
-                    alert(data);
-                    this.messagesWatson.push(data);
-                    this.messagesUsuario.push("");
+                    //alert(data);
+                    this.setMsgWatson(data);
+                    //window.location.href='#ancora';
+                    //this.messagesWatson.push(data);
+                    //this.messagesUsuario.push("");
                     }, err => {
                     console.log(err)
                     });
+                
                 });
         }
             
+         // window.scroll(0,0);  
             
-            this.chatBox = "";
         }
     
-    getUrlParametros(variable)
-{
+    getUrlParametros(variable){
+        
+    
+    
+
        var query = window.location.search.substring(1);
        var vars = query.split("&");
        for (var i=0;i<vars.length;i++) {
@@ -53,7 +78,18 @@ export class HomePage {
                if(pair[0] == variable){return pair[1];}
        }
        return(false);
-}
+        }
+    
+    setMsgWatson(conteudo){
+        var menssagem = {content: conteudo, position:'left', senderName: 'Watson'};
+        this.msg.push(menssagem);
+    }
+    
+    setMsgUsuario(conteudo){
+        var menssagem = {content: conteudo, position:'right', senderName: 'Usuario'};
+        this.msg.push(menssagem);
+        
+    }
         
     }
 
